7d6dbc707f1455f85c9d36df548a0ecd0b03596c
Osmanov Alexander <grandison@mail.ru>
2011-11-26 13:38:13 +0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,12 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) 
-require "rvm/capistrano" 
-require 'bundler/capistrano'
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
 set :application, "osmanov.me"
-set :repository,  "git@github.com:grandison/osmanov.me.git"
+set :repository,  "."
 set :deploy_to, "/home/ubuntu/#{application}"
 set :scm, :git
 set :user, "ubuntu"
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
-set :deploy_via, :remote_cache
-set :use_sudo, false
+set :location, "osmanov.me"
+role :app, location
+role :web, location
+role :db,  location, :primary => true
+set :deploy_via, :copy
 set :stack, :passenger

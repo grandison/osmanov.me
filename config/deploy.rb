dcf1c664e257be7930b527f4c138b9d50bcb04fb
Osmanov Alexander <grandison@mail.ru>
2011-11-29 22:15:54 +0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,10 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+require 'bundler/capistrano'
 set :application, "osmanov.me"
+set :rails_env, "production"
+set :rvm_ruby_string, 'ree'
+set :rvm_type, :user
 set :repository,  "."
 set :deploy_to, "/var/www/apps/#{application}"
 set :scm, :git
@@ -8,3 +14,4 @@ server "osmanov.me", :web, :app, :db, :primary => true
 set :deploy_via, :copy
 set :copy_exclude, [".git", "spec"]
 set :use_sudo, false
+set :rvm_ruby, "ruby-1.9.2-p180"
